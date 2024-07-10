ea53c762691b8528a919894857dc8ac296cfa6ed
Tarcio Rocha <tarcio.rocha@a3data.com.br>
2024-07-10 12:51:59 -0300

    ensure SearchProductsController calls use case with correct input

---
+++ src/application/controllers/search-products.controller.ts
@@ -0,0 +1,20 @@
+import { UseCase } from "@/domain/use-cases/use-case";
+import { Controller } from "./controller";
+import { HttpResponse, ok, serverError } from "../helpers/http";
+import { Input, Output } from "@/domain/use-cases/search-products.use-case";
+
+type HttpRequest = Input
+type Model = Output
+
+export class SearchProductsController implements Controller {
+  constructor(private readonly useCase: UseCase<Input, Model>) {}
+
+  async execute(httpRequest: HttpRequest): Promise<HttpResponse<Model>> {
+    try {
+      const httpResponse = await this.useCase.execute(httpRequest)
+      return ok(httpResponse)
+    } catch (error) {
+      return serverError()
+    }
+  }
+}
--- src/domain/use-cases/search-products.use-case.ts
+++ src/domain/use-cases/search-products.use-case.ts
@@ -1,11 +1,14 @@
+import { UseCase } from './use-case'
 import { ProductRepository } from '@/domain/contracts/repositories/product'
+import { ProductModel } from '@/domain/entities/product'
 
 export type Input = { perPage: number, page: number, id?: string, name?: string }
+export type Output = { data: ProductModel[], total: number }
 
-export class SearchProductsUseCase {
+export class SearchProductsUseCase implements UseCase<Input, Output> {
   constructor(private readonly productRepository: ProductRepository) {}
 
-  async execute(input: Input) {
+  async execute(input: Input): Promise<Output> {
     const result = await this.productRepository.search(input)
     return {
       data: result.data.map(product => product.toJSON()),

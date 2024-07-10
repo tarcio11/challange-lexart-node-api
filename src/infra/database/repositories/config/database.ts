b0decf2aff1d24b76394f06accb8266b689c7302
Tarcio Rocha <tarcio.rocha@a3data.com.br>
2024-07-10 16:54:28 -0300

    chore: Update npm dependency to latest stable version

--- src/infra/database/repositories/config/database.ts
+++ src/infra/database/repositories/config/database.ts
@@ -1,6 +1,7 @@
 import { Sequelize } from "sequelize-typescript";
 import { ProductModel } from "../sequelize/models/product.model";
 import { env } from "../../../../main/config/env";
+import pg from 'pg';
 
 const models = [
   ProductModel
@@ -12,4 +13,5 @@ export const sequelize = new Sequelize(env.database.url, {
   logging: false,
   models,
   ssl: true,
+  dialectModule: pg
 });

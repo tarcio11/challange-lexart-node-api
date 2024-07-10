6bf88cbc13a4a3a19b7d3f340515397a14b97bbd
Tarcio Rocha <tarcio.rocha@a3data.com.br>
2024-07-10 12:27:18 -0300

    ensure CreateProductController returns 201 on success

--- tests/application/controllers/create-product.controller.spec.ts
+++ tests/application/controllers/create-product.controller.spec.ts
@@ -20,4 +20,10 @@ describe('Controllers: CreateProductController', () => {
     expect(useCase.execute).toHaveBeenCalledWith({ name: 'valid_name', price: 10, stock: 10 })
     expect(useCase.execute).toHaveBeenCalledTimes(1)
   })
+
+  it('should return 201 on success', async () => {
+    const response = await sut.execute({ name: 'valid_name', price: 10, stock: 10 })
+
+    expect(response).toEqual({ statusCode: 201, data: null })
+  })
 })

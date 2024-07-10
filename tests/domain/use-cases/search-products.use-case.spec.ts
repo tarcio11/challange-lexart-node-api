5b6729f2784ff96bc76d8cf8337fc2d12468bd17
Tarcio Rocha <tarcio.rocha@a3data.com.br>
2024-07-10 00:14:03 -0300

    ensure SearchProductsUseCase returns correctly on success with result

--- tests/domain/use-cases/search-products.use-case.spec.ts
+++ tests/domain/use-cases/search-products.use-case.spec.ts
@@ -1,5 +1,6 @@
 import { mock, MockProxy } from 'jest-mock-extended'
 import { ProductRepository } from '@/domain/contracts/repositories/product'
+import { Product } from '@/domain/entities/product'
 
 type Input = { perPage: number, page: number, id?: string, name?: string }
 
@@ -19,9 +20,18 @@ describe('UseCase: SearchProducts', () => {
   let sut: SearchProductsUseCase
   let productRepository: MockProxy<ProductRepository>
   let input: Input
+  let product: Product
 
   beforeAll(() => {
     input = { perPage: 10, page: 1, id: 'any_id', name: 'any_name' }
+    product = new Product({
+      name: 'any_name',
+      price: 10,
+      stock: 1,
+      createdAt: new Date(),
+      id: 'any_id',
+      updatedAt: new Date()
+    })
     productRepository = mock()
     productRepository.search.mockResolvedValue({ data: [], total: 0 })
   })
@@ -50,5 +60,13 @@ describe('UseCase: SearchProducts', () => {
     const result = await sut.execute(input)
 
     expect(result).toEqual({ data: [], total: 0 })
+  })
+
+  it('should return correctly on success with result', async () => {
+    productRepository.search.mockResolvedValueOnce({ data: [product], total: 1 })
+
+    const result = await sut.execute(input)
+
+    expect(result).toEqual({ data: [product], total: 1 })
   })
 })

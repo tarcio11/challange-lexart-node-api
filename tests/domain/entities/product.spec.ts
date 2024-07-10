f3e7f8620c17d01ff1d82abebff8013dfbc50480
Tarcio Rocha <tarcio.rocha@a3data.com.br>
2024-07-09 21:42:38 -0300

    be able to create a new product with a required fields

--- tests/domain/entities/product.spec.ts
+++ tests/domain/entities/product.spec.ts
@@ -1,18 +1,27 @@
 type ProductModel = {
+  id?: string
   name: string
   price: number
   stock: number
+  createdAt?: Date
+  updatedAt?: Date
 }
 
 class Product {
+  id?: string
   name: string
   price: number
   stock: number
+  createdAt?: Date
+  updatedAt?: Date
 
   constructor (props: ProductModel) {
+    this.id = props.id
     this.name = props.name
     this.price = props.price
     this.stock = props.stock
+    this.createdAt = props.createdAt
+    this.updatedAt = props.updatedAt
   }
 }
 
@@ -29,6 +38,28 @@ describe('Domain: Product', () => {
       name: 'any_name',
       price: 10,
       stock: 10
+    })
+  });
+
+  it('should be able to create a new product with a required fields', () => {
+    const createdAt = new Date()
+    const updatedAt = new Date()
+    const sut = new Product({
+      id: 'any_id',
+      name: 'any_name',
+      price: 10,
+      stock: 10,
+      createdAt,
+      updatedAt
+    })
+
+    expect(sut).toEqual({
+      id: 'any_id',
+      name: 'any_name',
+      price: 10,
+      stock: 10,
+      createdAt,
+      updatedAt
     })
   });
 });

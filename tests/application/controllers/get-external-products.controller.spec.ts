04bd62d3e3a65bb74d6b24de1b3457c24cb9020b
Tarcio Rocha <tarcio.rocha@a3data.com.br>
2024-07-10 23:43:48 -0300

    ensure GetExternalProductsController calls use case with correct input

---
+++ tests/application/controllers/get-external-products.controller.spec.ts
@@ -0,0 +1,50 @@
+import { mock, MockProxy } from "jest-mock-extended";
+import { UseCase } from "../../../src/domain/use-cases/use-case";
+import { GetExternalProductsController } from "../../../src/application/controllers/get-external-products.controller";
+import { serverError } from "../../../src/application/helpers/http";
+import { ProductFakeBuilder } from "../../domain/fakes/product-fake.builder";
+import { Input, Output } from "../../../src/domain/use-cases/search-products.use-case";
+import { ProductModel } from "../../../src/domain/entities/product";
+
+describe('Controllers: GetExternalProductsController', () => {
+  let sut: GetExternalProductsController
+  let useCase: MockProxy<UseCase<Input, Output>>
+  let products: ProductModel[]
+
+  beforeAll(() => {
+    products = ProductFakeBuilder.theProducts(2).build().map(product => product.toJSON())
+    useCase = mock()
+    useCase.execute.mockResolvedValue({
+      data: products,
+      total: 2
+    })
+  })
+
+  beforeEach(() => {
+    sut = new GetExternalProductsController(useCase)
+  })
+
+  it('should call use case with correct input', async () => {
+    await sut.execute({ page: 1, perPage: 10 })
+
+    expect(useCase.execute).toHaveBeenCalledWith({ page: 1, perPage: 10, external: true })
+    expect(useCase.execute).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return 200 on success', async () => {
+    const response = await sut.execute({ page: 1, perPage: 10 })
+
+    expect(response).toEqual({ statusCode: 200, data: {
+      data: products,
+      total: 2
+    }})
+  })
+
+  it('should return 500 if use case throws', async () => {
+    useCase.execute.mockRejectedValueOnce(new Error('any_error'))
+
+    const response = await sut.execute({ page: 1, perPage: 10 })
+
+    expect(response).toEqual(serverError())
+  })
+})

86a8681b01877c4acb9930911e8faa6e9eafd161
Tarcio Rocha <tarcio.rocha@a3data.com.br>
2024-07-11 10:32:47 -0300

    chore: Update swagger-ui-dist and @types/swagger-ui-dist to latest versions

--- src/main/config/swagger.ts
+++ src/main/config/swagger.ts
@@ -1,6 +1,10 @@
 import { serve, setup } from 'swagger-ui-express'
 import { Express } from 'express'
 import swaggerConfig from '../documentation'
+import {
+  SwaggerUIBundle,
+  SwaggerUIStandalonePreset,
+} from "swagger-ui-dist";
 
 export default (app: Express): void => {
   app.use('/api/docs', serve, setup(swaggerConfig))

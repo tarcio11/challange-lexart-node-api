99a69d959bcd5af4a4778785eebcf958cb7ff698
Tarcio Rocha <tarcio.rocha@a3data.com.br>
2024-07-11 10:42:14 -0300

    chore: Update swagger configuration and dependencies

--- src/main/config/swagger.ts
+++ src/main/config/swagger.ts
@@ -1,17 +1,8 @@
 import { serve, setup, serveFiles } from 'swagger-ui-express'
 import { Express } from 'express'
 import swaggerConfig from '../documentation'
-import {
-  SwaggerUIBundle,
-  SwaggerUIStandalonePreset,
-} from "swagger-ui-dist";
 
-const CSS_URL = ' https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css ';
-
-const options = {
-  customCssUrl: CSS_URL,
-};
 
 export default (app: Express): void => {
-  app.use('/api/docs', serve, serveFiles(swaggerConfig, options), setup(swaggerConfig, options))
+  app.use('/api/docs', serve, setup(swaggerConfig))
 }
